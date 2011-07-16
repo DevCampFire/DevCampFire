428bdd0ea0a7a6df96bfc8ea54299fcf74efd5a7
Hugo Plante <hplante@gmail.com>
2011-07-16 15:15:54 -0400

    add SignIn and Register

---
+++ DevCampFire/RegisterViewController.h
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.h
+//  DevCampFire
+//
+//  Created by Hugo Plante on 11-07-16.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RegisterViewController : UIViewController {
+    
+}
+
+@end
